24fffa81d5ab3a8ddf9769855c73bf0846748192
hafsamohabah <hafsamohabah@gmail.com>
2026-09-08 11:46:52 +0300

    Implement display all toys

--- src/components/ToyContainer.jsx
+++ src/components/ToyContainer.jsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ToyCard from "./ToyCard";
 
 function ToyContainer() {
+  const [toys, setToys] = useState([]);
+
+  // Fetch all toys when the component first loads.
+  useEffect(() => {
+    fetch("http://localhost:6001/toys")
+      .then((response) => response.json())
+      .then((data) => setToys(data));
+  }, []);
+
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">
+      {toys.map((toy) => (
+        <ToyCard key={toy.id} toy={toy} />
+      ))}
+    </div>
   );
 }
 
