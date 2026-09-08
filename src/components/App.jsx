5b1f4b2a765c1a4be68e63a65b4ee9aff839eb49
hafsamohabah <hafsamohabah@gmail.com>
2026-09-08 12:15:58 +0300

    Implement add a toy

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import Header from "./Header";
 import ToyForm from "./ToyForm";
@@ -6,19 +6,35 @@ import ToyContainer from "./ToyContainer";
 
 function App() {
   const [showForm, setShowForm] = useState(false);
+  const [toys, setToys] = useState([]);
+
+  // Fetch all toys when the app first loads.
+  useEffect(() => {
+    fetch("http://localhost:6001/toys")
+      .then((response) => response.json())
+      .then((data) => setToys(data));
+  }, []);
 
   function handleClick() {
     setShowForm((showForm) => !showForm);
   }
 
+  // Add the newly created toy to the toys displayed on the page.
+  function handleToyAdded(newToy) {
+    setToys((currentToys) => [...currentToys, newToy]);
+  }
+
   return (
     <>
       <Header />
-      {showForm ? <ToyForm /> : null}
+
+      {showForm ? <ToyForm onToyAdded={handleToyAdded} /> : null}
+
       <div className="buttonContainer">
         <button onClick={handleClick}>Add a Toy</button>
       </div>
-      <ToyContainer />
+
+      <ToyContainer toys={toys} />
     </>
   );
 }
